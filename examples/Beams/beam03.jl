606dada7422ac10537ef8bd72a4830b6581c1cf4
Rob J Goedman <goedman@mac.com>
2014-12-07 15:55:53 -0800

    Added files

---
+++ examples/Beams/beam03.jl
@@ -0,0 +1,29 @@
+using Compat, LHR
+
+data = @Compat.Dict(
+  :support => [
+    (1, [0 0 0 0 0 0])],
+  :properties => [
+    (1, [2.0e6 1.0e6 1.0e6 3.0e5])],
+  :coordinates => [
+    (1, linspace(0, 4, 21))],
+  :node_numbering => [
+    (1, int(linspace(1, 20, 20))),
+    (2, int(linspace(2, 21, 20)))],
+  :loads => [
+    (21, [10000.0 1000.0 0.0 1000.0 0.0 0.0])]
+)
+
+data |> display
+println()
+
+m = FEmodel(20, 21, data)
+
+println("Displacements:")
+m.displacements |> display
+println()
+
+println("Actions:")
+m.actions |> display
+println()
+
