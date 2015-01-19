0da0fdbec05f530cc7af038dd0fdae287c3f57ac
Rob J Goedman <goedman@mac.com>
2015-01-19 09:33:43 -0800

    Removed printouts from test_ldlt.jl

--- test/test_ldlt.jl
+++ test/test_ldlt.jl
@@ -3,6 +3,7 @@ using CSoM
 A = Float64[16 4 8;4 5 -4;8 -4 22]
 b = Float64[4, 2, 5]
 
+#=
 A\b |> display
 println()
 
@@ -15,7 +16,10 @@ println()
 
 @show y = L\b
 println()
+=#
 
+(L, U) = lufac(A)
+y = L\b
 @assert y == [4.0,1.0,4.5]
 
 #=
