27b70077e6b282581d9f4695edcb9b2d45a6266f
Rob J Goedman <goedman@mac.com>
2015-02-03 11:06:58 -0800

    Plate example (p5.1)

--- examples/Plates/plate01.jl
+++ examples/Plates/plate01.jl
@@ -1,10 +1,31 @@
 using Compat, CSoM
 
+#=
+'plane'
+'triangle'   3   'x'
+2  2  1  1
+1.0e6  0.3
+0.0  0.5  1.0
+0.0 -0.5 -1.0
+5
+1 0 1  4 0 1  7 0 0  8 1 0  9 1 0
+3
+1  0.0 -0.25   2  0.0 -0.50   3  0.0 -0.25
+0
+=#
+
 data = @compat Dict(
+  :type => :plane,
+  :element => :triangle,
+  :nod => 3,
+  :direction => :x,
+  :nxe => 2,
+  :nye => 2,
+  :nip => 1,
   :support => [
     (1, [0 0 0 0 0 0])],
   :properties => [
-    (1, [2.0e6 1.0e6 1.0e6 3.0e5])],
+    (1, [1.0e6 0.3])],
   :coordinates => [
     (1, linspace(0, 4, 21))],
   :node_numbering => [
@@ -17,14 +38,5 @@ data = @compat Dict(
 data |> display
 println()
 
-println("Running Julia version:")
-@time m = FEmodel(20, 21, data)
-println()
-
-println("Displacements:")
-m.displacements |> display
-println()
-
-println("Actions:")
-m.actions |> display
+@time m = FEplate(20, 21, data)
 println()
