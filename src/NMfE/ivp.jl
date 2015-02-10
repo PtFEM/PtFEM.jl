e38a7ea59fc6a2eac0d115db0afecaf600a6b06b
Rob J Goedman <goedman@mac.com>
2015-02-10 13:56:26 -0800

    Added FEmodel test (p5.1.jl based)

--- src/NMfE/ivp.jl
+++ src/NMfE/ivp.jl
@@ -2,7 +2,7 @@ function euler(f::Function, x::Float64, y::Vector{Float64}, steps::Int64, h::Flo
   nsteps = steps + 1
   res = zeros(nsteps, length(y)+1)
   for i in 1:nsteps
-    res[i,:] = [x, [y[i] for i in 1:length(y)]]
+    res[i,:] = [x; [y[i] for i in 1:length(y)]]
     k0 = h * f(x, y)
     y += k0
     x += h
@@ -14,7 +14,7 @@ function modified_euler(f::Function, x::Float64, y::Vector{Float64}, steps::Int6
   nsteps = steps + 1
   res = zeros(nsteps, length(y)+1)
   for i in 1:nsteps
-    res[i,:] = [x, [y[i] for i in 1:length(y)]]
+    res[i,:] = [x; [y[i] for i in 1:length(y)]]
     k0 = h * f(x, y)
     k1 = h * f(x+h, y+k0)
     y += (k0+k1)/2.0
@@ -27,7 +27,7 @@ function mid_point_euler(f::Function, x::Float64, y::Vector{Float64}, steps::Int
   nsteps = steps + 1
   res = zeros(nsteps, length(y)+1)
   for i in 1:nsteps
-    res[i,:] = [x, [y[i] for i in 1:length(y)]]
+    res[i,:] = [x; [y[i] for i in 1:length(y)]]
     k0 = h * f(x, y)
     k1 = h * f(x+h/2.0, y+k0/2.0)
     y += k1
@@ -40,7 +40,7 @@ function runga_kutta_4(f::Function, x::Float64, y::Vector{Float64}, steps::Int64
   nsteps = steps + 1
   res = zeros(nsteps, length(y)+1)
   for i in 1:nsteps
-    res[i,:] = [x, [y[i] for i in 1:length(y)]]
+    res[i,:] = [x; [y[i] for i in 1:length(y)]]
     k0 = h * f(x, y)
     k1 = h * f(x+h/2.0, y+k0/2.0)
     k2 = h * f(x+h/2.0, y+k1/2.0)
--- test/test_csom1.jl
+++ test/test_csom1.jl
@@ -1,9 +1,9 @@
-using CSoM, Compat
+using CSoM
 
-data = @compat Dict(
+data = Dict(
   :element_type => Beam(20, 1, :x, Line(2)),
   :properties => [2.0e6 1.0e6 1.0e6 3.0e5;],
-  :x_coords => [linspace(0, 4, 21)],
+  :x_coords => linspace(0, 4, 21),
   :support => [
     (1, [0 0 0 0 0 0])
     ],
