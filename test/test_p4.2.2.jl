67e8a18b090f28720ad5b44f288ff4103b8e1d73
Rob J Goedman <goedman@mac.com>
2015-02-14 13:34:44 -0800

    Added pin_jointed.jl and a few more tests

---
+++ test/test_p4.2.2.jl
@@ -0,0 +1,29 @@
+using Compat, CSoM, Base.Test
+
+include(Pkg.dir("CSoM", "examples", "StaticEquilibrium", "FE4_2.jl"))
+
+data = @compat Dict(
+  # Frame(nels, nn, ndim, nst, nip, finite_element(nod, nodof))
+  :element_type => Frame(4, 5, 3, 1, 1, Line(2, 2)),
+  :properties => [5.0e5;],
+  :x_coords => [0.0, 1.25, 3.5, 4.0, 2.0],
+  :y_coords => [0.0, 3.0, 2.0, 1.0, 1.5],
+  :z_coords => [0.0, 0.0, 0.0, 0.0, 3.0],
+  :g_num => [1 2 3 4;5 5 5 5],
+  :support => [
+    (1, [0 0 0]),
+    (2, [0 0 0]),
+    (3, [0 0 0]),
+    (4, [0 0 0])
+    ],
+  :loaded_nodes => [
+    (5, [20.0 -20.0 30.0])],
+  :fixed_freedoms => [
+    (5, 2, -0.0005)],
+  :penalty => 1e19
+)
+
+
+@time m = FE4_2(data)
+
+@test round(m.displacements[:, 5], 7) == [0.0002569, -0.0005, 7.61e-5]
