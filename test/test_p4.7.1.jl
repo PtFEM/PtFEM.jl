23ba44a03d03e9ceef805e9cbfd1d701e1cb21e0
Rob J Goedman <goedman@mac.com>
2015-06-24 21:57:21 -0600

    Fixed test_p4.7.1.jl

--- test/test_p4.7.1.jl
+++ test/test_p4.7.1.jl
@@ -6,8 +6,8 @@ data = @compat Dict(
   # Plane(ndim, nst, nxe, nye, nip, direction, finite_element(nod, nodof), axisymmetric)
   :element_type => Plane(2, 3, 2, 2, 16, :x, Quadrilateral(4, 4), false),
   :properties => [10.92 0.3;],
-  :x_coords => linspace(0.0, 0.5, 3),
-  :y_coords => linspace(0.0, 0.5, 3),
+  :x_coords => collect(linspace(0.0, 0.5, 3)),
+  :y_coords => collect(linspace(0.0, 0.5, 3)),
   :thickness => 1.0,
   :support => [
     (1, [0 0 0 1]),
