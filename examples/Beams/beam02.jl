fd0c806907bbaa302a8e956ab05e315eecbfc7be
Rob J Goedman <goedman@mac.com>
2015-06-29 20:36:26 -0600

    Updating linspace occurrences

--- examples/Beams/beam02.jl
+++ examples/Beams/beam02.jl
@@ -6,7 +6,7 @@ data = @compat Dict(
   # Frame(nels, nn, ndim, nst, nip, finite_element(nod, nodof))
   :element_type => Frame(20, 21, 3, 1, 1, Line(2, 3)),
   :properties => [2.0e6 1.0e6 1.0e6 3.0e5;],
-  :x_coords => linspace(0, 4, 21),
+  :x_coords => collect(linspace(0, 4, 21)),
   :y_coords => zeros(21),
   :z_coords => zeros(21),
   :g_num => [
