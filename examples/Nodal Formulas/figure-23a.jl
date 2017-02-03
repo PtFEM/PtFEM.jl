dbdeb6dd5d6854996c3e3d64ae8f944c410ab885
Rob J Goedman <goedman@mac.com>
2017-02-03 07:23:23 -0800

    Testing with equivalent forces and moments

--- examples/Nodal Formulas/figure-23a.jl
+++ examples/Nodal Formulas/figure-23a.jl
@@ -3,21 +3,18 @@ using CSoM
 data = Dict(
   # Beam(ndim, nst, nxe, nip, direction, finite_element(nod, nodof), axisymmet
   :struc_el => Beam(2, 1, 4, 1, :x, Line(2, 2), false),
-  :properties => [4.0e4; 2.0e4],
-  :etype => [1, 1, 2, 2],
-  :x_coords => [0.0, 2.5, 5.0, 8.0, 10.0],
+  :properties => [1.0e4],
+  :x_coords => [0.0, 1.0, 2.0, 3.0, 4.0],
   :support => [
-    (1, [0 1]),
-    (4, [0 1])
+    (1, [0 0]),
+    (5, [0 0])
     ],
   :loaded_nodes => [
-  (2, [-20.0 0.0]),
-  (3, [-6.0 -3.0]),
-  (4, [-8.8 2.2]),
-  (5, [-1.2 0.5333])],
-  :fixed_freedoms => [
-    (1, 2, -0.001),
-    (3, 1, -0.005)],
+  (1, [-2.0 -4.0/12.0]),
+  (2, [-2.0 0.0]),
+  (3, [-2.0 0.0]),
+  (4, [-2.0 0.0]),
+  (5, [-2.0  4.0/12.0])],
   :penalty => 1e19
 )
 
@@ -28,6 +25,16 @@ nodal_forces = zeros(N, k)
 nodal_moments = zeros(N, k)
 equivalent_nodal_forces = zeros(N, k)
 equivalent_nodal_moments = zeros(N, k)
+
+enmv = zeros(N)
+enmv[1] = -4.0/12.0
+enmv[N] =  4.0/12.0
+enfv = [-2.0 for i in 1:N]
+
+lnodes = Array{Tuple{Int64,Array{Float64,2}},1}()
+for i in 1:N
+  push!(lnodes, (i, [enfv[i] enmv[i]]))
+end
 
 data |> display
 println()
