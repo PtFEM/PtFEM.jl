3384d02bb845e1f3c93563d7e440ed04844d6163
Rob J Goedman <goedman@mac.com>
2017-02-06 07:40:42 -0800

    Output using DataFrames on Julia 0.5

--- examples/4 Static Equilibrium/p4.3.2.jl
+++ examples/4 Static Equilibrium/p4.3.2.jl
@@ -20,10 +20,40 @@ println()
 @time m = FE4_3(data)
 println()
 
-println("Displacements:")
-m.displacements' |> display
-println()
+if VERSION.minor > 5
+  println("Displacements:")
+  m.displacements' |> display
+  println()
 
-println("Actions:")
-m.actions' |> display
-println()
+  println("Actions:")
+  m.actions' |> display
+  println()
+else
+  using DataFrames
+  dis_df = DataFrame(
+    translations = m.displacements'[:, 1],
+    rotations = m.displacements'[:, 2]
+  )
+  fm_df = DataFrame(
+    xl_Force = m.actions[1, :],
+    xl_Moment = m.actions[2, :],
+    xr_Force = m.actions[3, :],
+    xr_Moment = m.actions[4, :]
+  )
+  # Correct element forces and moments for equivalent nodal
+  # forces and moments introduced for loading between nodes
+  if :eq_nodal_forces_and_moments in keys(data)
+    eqfm = data[:eq_nodal_forces_and_moments]
+    k = data[:struc_el].fin_el.nod * data[:struc_el].fin_el.nodof
+
+    for t in eqfm
+      for i in 1:k
+        fm_df[t[1], i] -= t[2][i]
+      end
+    end
+  end
+    
+  display(dis_df)
+  println()
+  display(fm_df)
+end
