6ee3d016c9e2b49316bf0a401d424fc13d128974
Rob J Goedman <goedman@mac.com>
2017-04-12 16:27:11 -0700

    Added runexamples.jl

---
+++ test/runexamples.jl
@@ -0,0 +1,45 @@
+using PtFEM
+using Base.Test
+
+# write your own tests here
+
+if VERSION.minor == 3
+  isfile("example.vtu") && rm("example.vtu")
+  isfile("example_bin.vtu") && rm("example_bin.vtu")
+  isfile("example_compressed_bin.vtu") && rm("example_compressed_bin.vtu")
+end
+
+examples = [
+  "PtFEM/4 Static Equilibrium/Ex41.1.jl",
+  "PtFEM/4 Static Equilibrium/Ex41.2.jl",
+  "PtFEM/4 Static Equilibrium/Ex42.2.jl",
+  "PtFEM/4 Static Equilibrium/Ex43.1b.jl",
+  "PtFEM/4 Static Equilibrium/Ex44.2.jl",
+  "PtFEM/4 Static Equilibrium/Ex45.2.jl",
+  "PtFEM/4 Static Equilibrium/Ex46.2.jl",
+  "PtFEM/4 Static Equilibrium/Ex47.1.jl",
+  "PtFEM/5 Elastic Solids/Ex51.1.jl",
+  "PtFEM/5 Elastic Solids/Ex51.2.jl",
+  "PtFEM/5 Elastic Solids/Ex51.3.jl",
+  "PtFEM/5 Elastic Solids/Ex51.4.jl",
+  "PtFEM/5 Elastic Solids/Ex51.5.jl",
+  "PtFEM/5 Elastic Solids/Ex52.1.jl",
+  "PtFEM/5 Elastic Solids/Ex53.1.jl",
+  "PtFEM/5 Elastic Solids/Ex54.1.jl",
+  "PtFEM/5 Elastic Solids/Ex54.2.jl",
+  "PtFEM/5 Elastic Solids/Ex55.1.jl",
+  "PtFEM/5 Elastic Solids/Ex56.1.jl",
+  "PtFEM/5 Elastic Solids/Ex61.1.jl",
+  "PtFEM/5 Elastic Solids/Ex62.1.jl",
+  "PtFEM/6 Material Nonlinearity/Ex61.1.jl",
+  "PtFEM/6 Material Nonlinearity/Ex62.1.jl"
+]
+
+println("Running examples:")
+
+if VERSION.minor == 5
+  println("\nRunning Julia 0.5 examples\n")
+  for ex in examples
+    include(Pkg.dir("PtFEM")*"/$(ex)")
+  end
+end
