e24041ad859ea80f629abad397b60ee901b279e8
Rob J Goedman <goedman@mac.com>
2017-04-17 08:13:36 -0700

    Added VTK test programs (from WriteVTK.jl)

---
+++ vtk/multiblock.jl
@@ -0,0 +1,131 @@
+#!/usr/bin/env julia
+
+# This file tests generation of multiblock VTK files.
+# It also tests the non-default values of the optional parameters of vtk_grid,
+# i.e., compress=false and append=false.
+
+using WriteVTK
+import Compat: UTF8String, view
+const FloatType = Float32
+const vtm_filename_noext = "multiblock"
+
+function first_block_data()
+    const Ni, Nj, Nk = 10, 15, 20
+
+    x = zeros(FloatType, Ni, Nj, Nk)
+    y = copy(x)
+    z = copy(x)
+
+    # Just to test subarrays:
+    qall = zeros(FloatType, 3Ni, 2Nj, 2Nk)
+    q = view(qall, 1:Ni, 1:Nj, 1:Nk)
+
+    for k = 1:Nk, j = 1:Nj, i = 1:Ni
+        r::FloatType = 1 + (i - 1)/(Ni - 1)
+        th::FloatType = 3*pi/4 * (j-1) / (Nj-1)
+        x[i, j, k] = r * cos(th)
+        y[i, j, k] = r * sin(th)
+        z[i, j, k] = (k-1) / (Nk-1)
+        q[i, j, k] = k * exp(j) * sqrt(i)
+    end
+
+    return x, y, z, q
+end
+
+function second_block_data()
+    const Ni, Nj, Nk = 20, 16, 12
+
+    x = zeros(FloatType, Ni)
+    y = zeros(FloatType, Nj)
+    z = zeros(FloatType, Nk)
+
+    [x[i] =  1 +   (i-1) / (Ni-1) for i = 1:Ni]
+    [y[j] = -2 + 2*(j-1) / (Nj-1) for j = 1:Nj]
+    [z[k] =        (k-1) / (Nk-1) for k = 1:Nk]
+
+    q = zeros(FloatType, Ni, Nj, Nk)
+
+    for k = 1:Nk, j = 1:Nj, i = 1:Ni
+        q[i, j, k] = k * exp(j) * sqrt(i)
+    end
+
+    return x, y, z, q
+end
+
+function third_block_data()
+    # This is basically the same as in the unstructured example.
+    const Ni, Nj, Nk = 40, 50, 20
+    const dims = (Ni, Nj, Nk)
+
+    # Create points and point data.
+    pts = Array{FloatType}(3, Ni, Nj, Nk)
+    pdata = Array{FloatType}(Ni, Nj, Nk)
+
+    for k = 1:Nk, j = 1:Nj, i = 1:Ni
+        r = 1 + (i - 1)/(Ni - 1)
+        th = 3*pi/4 * (j - 1)/(Nj - 1)
+        pts[1, i, j, k] = r * cos(th)
+        pts[2, i, j, k] = r * sin(th)
+        pts[3, i, j, k] = -2 + (k - 1)/(Nk - 1)
+        pdata[i, j, k] = i*i + k*sqrt(j)
+    end
+
+    # Create cells (all hexahedrons in this case) and cell data.
+    const celltype = VTKCellTypes.VTK_HEXAHEDRON
+    cells = MeshCell[]
+    cdata = FloatType[]
+
+    for k = 2:Nk, j = 2:Nj, i = 2:Ni
+        # Define connectivity of cell.
+        inds = Array{Int32}(8)
+        inds[1] = sub2ind(dims, i-1, j-1, k-1)
+        inds[2] = sub2ind(dims, i  , j-1, k-1)
+        inds[3] = sub2ind(dims, i  , j  , k-1)
+        inds[4] = sub2ind(dims, i-1, j  , k-1)
+        inds[5] = sub2ind(dims, i-1, j-1, k  )
+        inds[6] = sub2ind(dims, i  , j-1, k  )
+        inds[7] = sub2ind(dims, i  , j  , k  )
+        inds[8] = sub2ind(dims, i-1, j  , k  )
+
+        # Define cell.
+        c = MeshCell(celltype, inds)
+
+        push!(cells, c)
+        push!(cdata, i*j*k)
+    end
+
+    return pts, cells, pdata, cdata
+end
+
+function main()
+    # Generate data.
+    x1, y1, z1, q1 = first_block_data()
+    x2, y2, z2, q2 = second_block_data()
+    points3, cells3, q3, c3 = third_block_data()
+
+    # Create multiblock file.
+    @time outfiles = vtk_multiblock(vtm_filename_noext) do vtm
+        # First block.
+        vtk = vtk_grid(vtm, x1, y1, z1; compress=false, append=false)
+        vtk_point_data(vtk, q1, "q_values")
+
+        # Second block.
+        vtk = vtk_grid(vtm, x2, y2, z2; compress=false)
+        vtk_point_data(vtk, q2, "q_values")
+
+        # Third block.
+        vtk = vtk_grid(vtm, points3, cells3; append=false)
+        vtk_point_data(vtk, q3, "q_values")
+        vtk_cell_data(vtk, c3, "c_values")
+    end
+    println("Saved:  ", join(outfiles, "  "))
+
+    return outfiles::Vector{UTF8String}
+end
+
+ProjDir = dirname(@__FILE__)
+cd(ProjDir*"/output") do
+
+  main()
+  
+end
---
+++ vtk/pvdCollection.jl
@@ -0,0 +1,71 @@
+#!/usr/bin/env julia
+
+using WriteVTK
+using Compat: UTF8String, view
+const FloatType = Float32
+const vtk_filename_noext = "collection"
+
+function main()
+    # Define grid.
+    const Ni, Nj, Nk, Nt = 20, 30, 40, 4
+
+    x = zeros(FloatType, Ni)
+    y = zeros(FloatType, Nj)
+    z = zeros(FloatType, Nk)
+
+    [x[i] = i*i/Ni/Ni for i = 1:Ni]
+    [y[j] = sqrt(j/Nj) for j = 1:Nj]
+    [z[k] = k/Nk for k = 1:Nk]
+
+    # Create some scalar and vectorial data that decays exponentially in
+    # time
+    p = zeros(FloatType, Ni, Nj, Nk, Nt)
+    q = zeros(FloatType, Ni, Nj, Nk, Nt)
+    vec = zeros(FloatType, 3, Ni, Nj, Nk, Nt)
+
+    for t = 1:Nt, k = 1:Nk, j = 1:Nj, i = 1:Ni
+        p[i, j, k, t] = exp(-t)*(i*i + k)
+        q[i, j, k, t] = exp(-t)*k*sqrt(j)
+        vec[1, i, j, k] = exp(-t)*i
+        vec[2, i, j, k] = exp(-t)*j
+        vec[3, i, j, k] = exp(-t)*k
+    end
+
+    # Create some scalar data at grid cells.
+    # Note that in structured grids, the cells are the hexahedra formed between
+    # grid points.
+    cdata = zeros(FloatType, Ni-1, Nj-1, Nk-1, Nt)
+    for t = 1:Nt, k = 1:Nk-1, j = 1:Nj-1, i = 1:Ni-1
+        cdata[i, j, k,t] = exp(-t)*(2i + 3k * sin(3*pi * (j-1) / (Nj-2)))
+    end
+
+    # Test extents (this is optional!!)
+    ext = [0, Ni-1, 0, Nj-1, 0, Nk-1] + 42
+
+    # Initialise pvd container file
+    @time outfiles = paraview_collection(vtk_filename_noext) do pvd
+        # Create files for each time-step and add them to the collection
+        for it = 0:Nt-1
+            vtk = vtk_grid(@sprintf("%s_%02i", vtk_filename_noext, it), x, y, z;
+                           extent=ext)
+            # Add data for current time-step
+            vtk_point_data(vtk, view(p, :, :, :, it+1), "p_values")
+            vtk_point_data(vtk, view(q, :, :, :, it+1), "q_values")
+            vtk_point_data(vtk, view(vec, :, :, :, :, it+1), "myVector")
+            vtk_cell_data(vtk, view(cdata, :, :, :, it+1), "myCellData")
+            vtk_save(vtk)
+            collection_add_timestep(pvd, vtk, Float64(it+1))
+        end
+    end
+    println("Saved:  ", join(outfiles, "  "))
+
+    return outfiles::Vector{UTF8String}
+end
+
+ProjDir = dirname(@__FILE__)
+cd(ProjDir*"/output") do
+
+  main()
+  
+end
+
