1295be4cd206365eea857232d4d0d88463fe49da
Rob J Goedman <goedman@mac.com>
2017-05-02 06:40:58 -0700

    Created Types subdir

---
+++ src/PtFEM/Types/finite_elements.jl
@@ -0,0 +1,163 @@
+using Compat
+
+### Finite elements used to approximate fin_el ###
+
+"""
+## FiniteElement
+
+Abstract finite element type.
+
+### Type
+```julia
+abstract FiniteElement
+```
+
+### Subtypes
+```julia
+* Line::FiniteElement          : 1D Line(nod, nodof)
+* Triangle::FiniteElement      : 2D Triangle(nod, nodof)
+* Quadrilateral::FiniteElement : 2D Quadrilateral(nod, nodof)
+* Hexahedron::FiniteElement    : 3D Hexahedron(nod, nodof)
+* Tetrahedron::FiniteElement   : 3D Tetrahedron(nod, nodof)
+```
+"""
+@compat abstract type FiniteElement end
+
+"""
+## Line (Interval)
+
+1D type finite element
+
+### Constructor
+```julia
+Line(nod, nodof)
+Line(nodof)
+```
+
+### Arguments
+```julia
+* nod::Int64       : Number of nodes for finite element, defaults to 2
+* nodof::Int64     : Number of degrees of freedom per node
+```
+
+### Related help
+```julia
+?FiniteElement      : Help on finite element types
+```
+
+"""
+immutable Line <: FiniteElement
+  nod::Int64
+  nodof::Int64
+end
+Line(nodof::Int64) = Line(2, nodof)
+
+"""
+## Triangle
+
+2D type finite element
+
+### Constructor
+```julia
+Triangle(nod, nodof)
+```
+
+### Arguments
+```julia
+* nod::Int64       : Number of nodes for finite element (3, 6, 10, 15)
+* nodof::Int64     : Number of degrees of freedom per node
+```
+
+### Related help
+```julia
+?FiniteElement      : Help on finite element types
+```
+
+"""
+immutable Triangle <: FiniteElement
+  nod::Int64
+  nodof::Int64
+end
+
+"""
+## Quadrilateral
+
+2D type finite element
+
+### Constructor
+```julia
+Quadrilateral(nod, nodof)
+```
+
+### Arguments
+```julia
+* nod::Int64       : Number of nodes for finite element (4, 8, 9)
+* nodof::Int64     : Number of degrees of freedom per node
+```
+
+### Related help
+```julia
+?FiniteElement      : Help on finite element types
+```
+
+"""
+immutable Quadrilateral <: FiniteElement
+  nod::Int64
+  nodof::Int64
+end
+
+"""
+## Tetrahedron
+
+3D type finite element
+
+### Constructor
+```julia
+Tetrahedron(nod, nodof)
+Tetrahedron(nodof)
+```
+
+### Arguments
+```julia
+* nod::Int64       : Number of nodes for finite element (defaults to 4)
+* nodof::Int64     : Number of degrees of freedom per node
+```
+
+### Related help
+```julia
+?FiniteElement      : Help on finite element types
+```
+
+"""
+immutable Tetrahedron <: FiniteElement
+  nod::Int64
+  nodof::Int64
+end
+Tetrahedron(nodof::Int64) = Tetrahedron(4, nodof)
+
+"""
+## hexahedron
+
+3D type finite element
+
+### Constructor
+```julia
+Hexahedron(nod, nodof)
+```
+
+### Arguments
+```julia
+* nod::Int64       : Number of nodes for finite element (8, 14, 20)
+* nodof::Int64     : Number of degrees of freedom per node
+```
+
+### Related help
+```julia
+?FiniteElement      : Help on finite element types
+```
+
+"""
+immutable Hexahedron <: FiniteElement
+  nod::Int64
+  nodof::Int64
+end
---
+++ src/PtFEM/Types/structural_elements.jl
@@ -0,0 +1,151 @@
+### Top level structural component ###
+
+"""
+## StructuralElement
+
+Abstract structural element type.
+
+### Type
+```julia
+abstract StructuralElement
+```
+
+### Subtypes
+```julia
+* Rod::StructuralElement          : Rod(nxe, np_types, nip, fin_el)
+* Beam::StructuralElement         : Beam(nod, nodof)
+* Frame::StructuralElement        : Frame(nod, nodof)
+* Plane::StructuralElement        : Plane(nod, nodof)
+* Solid::StructuralElement        : Solid(nod, nodof)
+* GenericSolid::StructuralElement : GenericSolid(nod, nodof)
+```
+
+### Related help
+```julia
+?FiniteElement                   : Show all finite elements
+?Rod                              : Help on Rod structural element
+?Beam                             : Help on Beam structural element
+?Frame                            : Help on Frame structural element
+?Plane                            : Help on Plane structural element
+?Solid                            : Help on Solid structural element
+?GenericSolid                     : Help on GenericSolid structural element
+```
+"""
+@compat abstract type StructuralElement end
+
+"""
+## Rod
+
+Concrete 1D structural element with only axial stresses.
+
+### Constructor
+```julia
+Rod(nels, np_types, nip, fin_el)
+```
+
+### Arguments
+```julia
+* nels::Int64             : Number of fin_els (stored in field nxe)
+* np_types::Int64         : Number of different property types
+* nip::Int64              : Number of integration points
+* fin_el::FiniteElement   : Line(nod, nodof)
+```
+
+### Related help
+```julia
+?StructuralElement  : Help on structural elements
+?FiniteElement      : Help on finite element types
+?Line               : Help on a Line finite element
+```
+
+"""
+immutable Rod <: StructuralElement  # Axial stresses only structural element
+  nxe::Int64                        # Number of fin_els
+  np_types::Int64                   # Number of property types
+  nip::Int64                        # Number of integration points per fin_el
+  fin_el::FiniteElement             # Finite fin_el type used (must be Line(...))
+end
+
+"""
+## Beam
+
+Concrete structural element with transverse and moment loading.
+
+### Constructor
+```julia
+Beam(ndim, nip, fin_el)
+```
+
+### Arguments
+```julia
+* ndim::Int64             : Number of dimensions
+* nst::Int64              : Number of stress terms
+* nxe::Int64              : Number of different property types
+* nip::Int64              : Number of integration points
+* direction::Symbol       : Number of integration points
+* fin_el::FiniteElement   : Line(nod, nodof)
+* axisymmetric::Bool      : Axisymmetric if true
+```
+
+### Related help
+```julia
+?StructuralElement  : Help on structural elements
+?FiniteElement      : Help on finite element types
+?Line               : Help on a Line finite element
+```
+
+"""
+type Beam <: StructuralElement    # 1D Beam structural element
+  ndim::Int64                     # Number of dimensions (1,2 or 3)
+  nst::Int64                      # Number of stress terms
+  nxe::Int64                      # Number of fin_els in x direction
+  nip::Int64                      # Number of integration points per fin_el
+  direction::Symbol               # Node numbering direction
+  fin_el::FiniteElement           # Finite fin_el type used
+  axisymmetric::Bool              # Axisymmetric
+end
+
+# Frame(nels, nn, ndim, finite_element(nod, nodof))
+type Frame <: StructuralElement
+  nels::Int64                     # Number of elemnt
+  nn::Int64                       # Number of nodes
+  ndim::Int64                     # Number of dimensions
+  nst::Int64                      # Number of stress terms
+  nip::Int64                      # Number of integration points per fin_el
+  fin_el::FiniteElement           # Finite fin_el type used
+end
+
+type Plane <: StructuralElement
+  ndim::Int64                     # Number of dimensions (1,2 or 3)
+  nst::Int64                      # Number of stress terms
+  nxe::Int64                      # Number of fin_els in x direction
+  nye::Int64                      # Number of fin_els in y direction
+  nip::Int64                      # Number of integration points
+  direction::Symbol               # Node numbering direction
+  fin_el::FiniteElement           # Finite fin_el type used
+  axisymmetric::Bool              # Axisymmetric
+end
+
+type Solid <: StructuralElement
+  ndim::Int64                     # Number of dimensions (1,2 or 3)
+  nst::Int64                      # Number of stress terms
+  nxe::Int64                      # Number of fin_els in x direction
+  nye::Int64                      # Number of fin_els in y direction
+  nze::Int64                      # Number of fin_els in y direction
+  nip::Int64                      # Number of integration points
+  fin_el::FiniteElement           # Finite fin_el type used
+end
+
+type GenericSolid <: StructuralElement
+  ndim::Int64                     # Number of dimensions (1,2 or 3)
+  nst::Int64                      # Number of stress terms
+  nels::Int64                     # Number of fin_els
+  nn::Int64                       # Number of nodes
+  nip::Int64                      # Number of integration points
+  fin_el::FiniteElement           # Finite fin_el type used
+  axisymmetric::Bool              # Axisymmetric
+end
+
+type UnknownStructuralElement <: StructuralElement
+end
+
