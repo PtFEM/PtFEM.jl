174d5c57e369edd3cfa750ed50f5c30e7af88501
Rob J Goedman <goedman@mac.com>
2017-06-11 12:30:52 -0700

    Turn type into struct or mutable struct

--- src/graphics/vtkrecipes/exportVTK_XML.jl
+++ src/graphics/vtkrecipes/exportVTK_XML.jl
@@ -2,11 +2,11 @@ using LightXML
 using Codecs
 using Compat
 
-type VTKNode
+struct VTKNode
   coords::Vector{Float64}
 end
 
-type VTKElement
+struct VTKElement
   vertices::Vector{Int}
   vtknum::Int
 end
@@ -21,7 +21,7 @@ function add_data!{T <: AbstractVTKXMLBinary}(vtkxml::T, data)
     write(vtkxml.buffer, data)
 end
 
-type VTKXMLBinaryCompressed <: AbstractVTKXMLBinary
+struct VTKXMLBinaryCompressed <: AbstractVTKXMLBinary
     buffer::IOBuffer
 end
 VTKXMLBinaryCompressed() = VTKXMLBinaryCompressed(IOBuffer())
@@ -53,7 +53,7 @@ function write_data!(vtkxml::VTKXMLBinaryUncompressed, xmlele::XMLElement)
 end
 
 
-type VTKXMLASCII <: AbstractVTKXML
+struct VTKXMLASCII <: AbstractVTKXML
     buffer::IOBuffer
 end
 VTKXMLASCII() = VTKXMLASCII(IOBuffer())
