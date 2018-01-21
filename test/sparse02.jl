aec7e23a8a92eb241d9bb148d19613849bc48f17
Rob J Goedman <goedman@mac.com>
2018-01-21 09:49:25 -0500

    Updated test/sparse02.jl

--- test/sparse02.jl
+++ test/sparse02.jl
@@ -1,3 +1,12 @@
+using Compat
+
+if VERSION.minor == 7
+  @eval using LinearAlgebra, SparseMatrices
+end
+
+ProjDir = dirname(@__FILE__)
+cd(ProjDir) do
+
 A = Float64[10 1 -5;-20 3 20;5 3 5]
 b = Float64[1; 2; 6]
 
@@ -12,7 +21,7 @@ println()
 @show As = sparse(A)
 println()
 
-@show As\l
+@show As \ y
 println()
 
 @show F = lufact(As)
@@ -33,7 +42,16 @@ println()
 @show sparse((F[:Rs] .* A)[F[:p], F[:q]])
 println()
 
-@show PtFEM.lufac(A)
+@show lufact(A)
 println()
 
-cd(old)
+F = lufact(A)
+
+if VERSION.minor == 7
+  F.L * F.U == A[p, :]
+else
+  F[:L]* F[:U] == A[F[:p], :]
+end
+  
+
+end
