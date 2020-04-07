bcb98897ca557b0c811198d9dfbe59b9805dcef9
Rob J Goedman <goedman@mac.com>
2020-04-07 10:14:13 -0400

    Release 1.2.3

--- src/PtFEM.jl
+++ src/PtFEM.jl
@@ -68,7 +68,7 @@ include("PtFEM/Geom/hexahedron_xz.jl")
 
 #include("graphics/vtkrecipes/vtk.jl")
 #include("graphics/vtkrecipes/exportVTK_XML.jl")
-include("graphics/plotrecipes/mesh.jl")
+#include("graphics/plotrecipes/mesh.jl")
 
 include("PtFEM/Deprecated/fkdiag.jl")
 include("PtFEM/Deprecated/fsparv.jl")
